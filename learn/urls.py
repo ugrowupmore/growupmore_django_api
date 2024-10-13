5392253f3610b6b56de7ce17aed41f8b41586c72
Girish Chaudhary <ugrowupmore@gmail.com>
2024-10-13 21:29:22 +0530

    learn url.py is updated

--- learn/urls.py
+++ learn/urls.py
@@ -1,15 +1,15 @@
 # # learn/urls.py
 
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from learn.views import SubjectViewSet, ChapterViewSet, TopicViewSet, SubTopicViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from learn.views import SubjectViewSet, ChapterViewSet, TopicViewSet, SubTopicViewSet
 
-# router = DefaultRouter()
-# router.register(r'subjects', SubjectViewSet)
-# router.register(r'chapters', ChapterViewSet)
-# router.register(r'topics', TopicViewSet)
-# router.register(r'subtopics', SubTopicViewSet)
+router = DefaultRouter()
+router.register(r'subjects', SubjectViewSet)
+router.register(r'chapters', ChapterViewSet)
+router.register(r'topics', TopicViewSet)
+router.register(r'subtopics', SubTopicViewSet)
 
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
+urlpatterns = [
+    path('', include(router.urls)),
+]
