03239ef84e33e8a6c126a5dbebe4db7a4698a760
Girish Chaudhary <ugrowupmore@gmail.com>
2024-10-13 21:23:02 +0530

    setting errro solved

--- GrowUpMore/settings.py
+++ GrowUpMore/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
     'rest_framework',
     'django_filters',
     'utils',
