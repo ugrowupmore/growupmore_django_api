1d9b1e584fd5547ea22886f399d3c06c0bd816de
Girish Chaudhary <ugrowupmore@gmail.com>
2024-10-13 21:30:44 +0530

    Learn files updated

--- learn/filters.py
+++ learn/filters.py
@@ -1,24 +1,24 @@
-# # learn/filters.py
+# learn/filters.py
 
-# from utils.filters import BaseFilterSet, get_non_image_fields
-# from learn.models import Subject, Chapter, Topic, SubTopic
+from utils.filters import BaseFilterSet, get_non_image_fields
+from learn.models import Subject, Chapter, Topic, SubTopic
 
-# class SubjectFilter(BaseFilterSet):
-#     class Meta(BaseFilterSet.Meta):
-#         model = Subject
-#         fields = get_non_image_fields(Subject)
+class SubjectFilter(BaseFilterSet):
+    class Meta(BaseFilterSet.Meta):
+        model = Subject
+        fields = get_non_image_fields(Subject)
 
-# class ChapterFilter(BaseFilterSet):
-#     class Meta(BaseFilterSet.Meta):
-#         model = Chapter
-#         fields = get_non_image_fields(Chapter)
+class ChapterFilter(BaseFilterSet):
+    class Meta(BaseFilterSet.Meta):
+        model = Chapter
+        fields = get_non_image_fields(Chapter)
 
-# class TopicFilter(BaseFilterSet):
-#     class Meta(BaseFilterSet.Meta):
-#         model = Topic
-#         fields = get_non_image_fields(Topic)
+class TopicFilter(BaseFilterSet):
+    class Meta(BaseFilterSet.Meta):
+        model = Topic
+        fields = get_non_image_fields(Topic)
 
-# class SubTopicFilter(BaseFilterSet):
-#     class Meta(BaseFilterSet.Meta):
-#         model = SubTopic
-#         fields = get_non_image_fields(SubTopic)
+class SubTopicFilter(BaseFilterSet):
+    class Meta(BaseFilterSet.Meta):
+        model = SubTopic
+        fields = get_non_image_fields(SubTopic)
