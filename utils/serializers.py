a6ad80ebcbc29605a9544c9226ffd3e1988e2ebc
Girish Chaudhary <girishinindia@gmail.com>
2024-10-21 14:40:48 +0530

    update serilizer

--- utils/serializers.py
+++ utils/serializers.py
@@ -8,29 +8,34 @@ class SlugValidationMixin:
     slug_field = 'slug'       # Name of the slug field in the model
 
     def generate_slug(self, validated_data):
-        values = []
-
-        # Handle foreign key fields properly (like 'country__iso3')
-        for field in self.slug_source_field:
-            if '__' in field:
-                related_field, attr = field.split('__')
-                related_obj = validated_data.get(related_field)
-                if related_obj:
-                    values.append(getattr(related_obj, attr, ''))
-            else:
-                values.append(validated_data.get(field, '') or '')
-
-        # Join the values and generate a slug
-        return slugify('-'.join(str(value) for value in values if value))
+        if isinstance(self.slug_source_field, str):
+            value = validated_data.get(self.slug_source_field, '') or ''
+            return slugify(value)
+        elif isinstance(self.slug_source_field, (list, tuple)):
+            values = [str(validated_data.get(field, '') or '') for field in self.slug_source_field]
+            return slugify('-'.join(values))
+        else:
+            raise ValueError("slug_source_field must be a string or list/tuple of strings")
 
     def validate(self, attrs):
         ModelClass = self.Meta.model
 
         # Check if the model has the slug field and slug_source_field is specified
         if self.slug_field in [field.name for field in ModelClass._meta.get_fields()] and self.slug_source_field:
-            regenerate_slug = True if not self.instance else False
+            # Determine whether to regenerate the slug
+            regenerate_slug = False
+            if self.instance:
+                # We're updating an existing instance
+                for field in self.slug_source_field:
+                    if attrs.get(field) != getattr(self.instance, field):
+                        regenerate_slug = True
+                        break
+            else:
+                # We're creating a new instance
+                regenerate_slug = True
 
             if regenerate_slug:
+                # Generate the slug
                 slug = self.generate_slug(attrs)
                 attrs[self.slug_field] = slug
 
@@ -39,10 +44,21 @@ class SlugValidationMixin:
                 if self.instance:
                     queryset = queryset.exclude(pk=self.instance.pk)
                 if queryset.exists():
-                    raise serializers.ValidationError(f"Slug '{slug}' already exists.")
+                    source_fields = self.slug_source_field
+                    if isinstance(source_fields, (list, tuple)):
+                        field_names = ', '.join(source_fields)
+                        field_values = ', '.join(str(attrs.get(field, '')) for field in source_fields)
+                        error_message = f"{field_names} ('{field_values}') already exists."
+                    else:
+                        field_names = source_fields
+                        field_values = str(attrs.get(source_fields, ''))
+                        error_message = f"{field_names} ('{field_values}') already exists."
 
+                    raise serializers.ValidationError({
+                        field_names: error_message
+                    })
             else:
+                # Keep the existing slug if the source fields haven't changed
                 attrs[self.slug_field] = getattr(self.instance, self.slug_field)
 
         return super().validate(attrs)
-
