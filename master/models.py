60ec2897397bfa15f52198d3a600514abc8b7253
Girish Chaudhary <girishinindia@gmail.com>
2024-10-21 15:13:33 +0530

    model updated

--- master/models.py
+++ master/models.py
@@ -28,6 +28,9 @@ class Country(SoftDeleteModel):
     founded_date = models.DateField(null=True, blank=True)
     website = models.TextField(default='NA')   
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = '"master"."countries"'
         indexes = [
@@ -56,10 +59,9 @@ class Country(SoftDeleteModel):
         if not self.is_deleted:            
             State.objects.filter(country=self).update(is_deleted=False)
             City.objects.filter(country=self).update(is_deleted=False)   
-            Employee.objects.filter(country=self).update(is_deleted=False)      
+            Employee.objects.filter(country=self).update(is_deleted=False)   
 
-    def __str__(self):
-        return self.name
+    
 
 # State model
 class State(SoftDeleteModel):
